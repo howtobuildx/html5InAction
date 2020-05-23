a1fcab1e1e07d90a3ee6aed58b6750ac90b1aa49
xbuilder <htbuildx@gmail.com>
2020-05-23 15:30:03 +0100

    learnt about DOM interactions and the Document, Element, and Node Interface

---
+++ p2/p2.js
@@ -0,0 +1,35 @@
+const mainHeading = document.querySelector('#main-heading');
+const otherHeading = document.querySelector('#other-heading');
+const excitedText = document.createElement('span');
+
+excitedText.textContent = '!!!';
+mainHeading.appendChild(excitedText);
+otherHeading.appendChild(excitedText);
+
+const myCustomDiv = document.createElement('div');
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is paragraph number ' + i;
+
+    newElement.addEventListener('click', function respondToTheClick(evt) {
+        console.log(evt, 'A paragraph was clicked.', evt.target);
+        evt.target.remove();
+    });
+
+    myCustomDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myCustomDiv);
+
+function beforePageExit(event) {
+	console.log(event)
+	event.preventDefault()
+	event.returnValue = "Hey hey are you sure you want to leave this page?"
+
+}
+
+
+document.addEventListener("beforeunload", beforePageExit)
+
+
